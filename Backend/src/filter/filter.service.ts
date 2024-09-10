97eebe7bc654b99758f6e260137a24aecadfb002
iliesska <58472432+iliesskalli@users.noreply.github.com>
2024-09-10 20:13:43 +0200

    Update filter.service.ts

--- Backend/src/filter/filter.service.ts
+++ Backend/src/filter/filter.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Item } from '../item/item.interface'; // Assurez-vous que le chemin et le nom du fichier sont corrects
+import { Item } from '../item/item.iterface'; // Assurez-vous que le chemin et le nom du fichier sont corrects
 
 @Injectable()
 export class FilterService {
